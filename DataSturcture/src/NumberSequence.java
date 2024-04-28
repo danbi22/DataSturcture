a318c241f76735455458e3cb7bc587b06d192a28
YUDAHAN <dbekgks98@gmail.com>
2024-04-29 00:14:51 +0900

    수열(재귀)

---
+++ DataSturcture/src/NumberSequence.java
@@ -0,0 +1,10 @@
+public class NumberSequence {
+
+    public static void main(String[] args) {
+        // aₙ = 5an-1 + 3, a1 = 0
+        int a = 'ₙ';
+        char b = 8345;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
