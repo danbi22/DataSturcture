625bd2dfcbfc8055c3d66f18d95d6893ea922df5
YUDAHAN <dbekgks98@gmail.com>
2024-04-29 00:48:42 +0900

    수열(재귀)

--- DataSturcture/src/NumberSequence.java
+++ DataSturcture/src/NumberSequence.java
@@ -1,10 +1,21 @@
 public class NumberSequence {
+    /**
+     *  다음 수열의 n번째 항을 구하는 재귀 알고리즘
+     *  aₙ = 5aₙ₋₁ + 3, a₁ = 0
+     *
+     * @param n int
+     *
+     * @return int
+     */
+    public static int getNthTerm(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        return getNthTerm(n-1) * 5 + 3;
+    }
 
     public static void main(String[] args) {
-        // aₙ = 5an-1 + 3, a1 = 0
-        int a = 'ₙ';
-        char b = 8345;
-        System.out.println(a);
-        System.out.println(b);
+        int result = getNthTerm(3);
+        System.out.println(result);
     }
 }
